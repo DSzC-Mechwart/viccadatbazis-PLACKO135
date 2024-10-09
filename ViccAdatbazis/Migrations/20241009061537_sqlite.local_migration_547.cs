cabeb68efaa7e2093167679d4c2dcc2b9506a1c7
Placko135 <papplaszlolevente@gmail.com>
2024-10-09 08:47:11 +0200

    trying but dunno what I'm doin

---
+++ ViccAdatbazis/Migrations/20241009061537_sqlite.local_migration_547.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ViccAdatbazis.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqlitelocal_migration_547 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
